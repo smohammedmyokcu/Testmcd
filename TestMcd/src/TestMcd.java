2530acef10989516ebc961d2e798da0b5c4d3a7a
smohammedmyokcu <113270721+smohammedmyokcu@users.noreply.github.com>
2022-09-21 09:57:42 -0500

    Update TestMcd.java

--- TestMcd/src/TestMcd.java
+++ TestMcd/src/TestMcd.java
@@ -26,7 +26,7 @@ public class TestMcd {
 	System.out.println("The content of array is");
 	for(int i = 0; i < arr.length; i++)
 	System.out.print(" "+arr[i]);
-	System.out.println();
+	System.out.println("\n********************************");
 	
 
 	System.out.println("The largest number is:"+largest(arr));
